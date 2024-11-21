da6dfd4d2b0314a489f0b6dbace1bce2a9ba6d13
lcordier <louis.cordier--temple@cern.ch>
2024-11-21 15:03:37 +0100

    Add example finder

---
+++ spikes/example_finder/examples/file1.py
@@ -0,0 +1,10 @@
+# Keywords: serial, loopback, communication
+# Description: Description of file 1, this is such a niiiice example finder
+
+
+def printfilename():
+    print("file1")
+
+
+if __name__ == "__main__":
+    printfilename
