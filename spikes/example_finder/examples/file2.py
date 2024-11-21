3f7a87d63dfc7b956ffe597dd01615760e4311e4
lcordier <louis.cordier--temple@cern.ch>
2024-11-21 15:03:37 +0100

    Add example finder

---
+++ spikes/example_finder/examples/file2.py
@@ -0,0 +1,9 @@
+# Keywords: serial, acquisition, configuration
+# Description: This is the description line of file2
+
+def printfilename():
+    print("file2")
+
+
+if __name__ == "__main__":
+    printfilename
