17655e3278e26f2e0f714489ecc0d7d0867969da
Alvaro Martinez Landete <alvaro.martinez.landete@cern.ch>
2024-11-22 14:42:09 +0100

    added timer but doesn't work well yet, updates seemingly randomly

--- spikes/GUI/simple ModelView.py
+++ spikes/GUI/simple ModelView.py
@@ -1,8 +1,10 @@
 # https://doc.qt.io/qt-6/modelview.html
 
 from PyQt6.QtWidgets import QApplication, QTableView
-from PyQt6.QtCore import QAbstractTableModel, Qt
+from PyQt6.QtCore import QAbstractTableModel, Qt, QTime
 import sys
+from PyQt6.QtGui import QFont, QColor
+from PyQt6.QtCore import QTimer
 
 
 class MyModel(QAbstractTableModel):
@@ -12,6 +14,17 @@ class MyModel(QAbstractTableModel):
         self.columnCount
         pass
 
+    def startTimer(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.timerEvent)
+        self.timer.start(1000)  # Timer set to 1 second intervals
+
+    def timerEvent(self):
+        # Emit dataChanged signal to update the view
+        top_left = self.index(0, 1)
+        bottom_right = self.index(0, 1)
+        self.dataChanged.emit(top_left, bottom_right)
+
     def rowCount(self, parent=None):
         return 4  # Example row count
     
@@ -19,9 +32,25 @@ class MyModel(QAbstractTableModel):
         return 3  # Example column count
     
     def data(self, index, role):
-        if role == Qt.ItemDataRole.DisplayRole:
-            return f"Row {index.row()}, Column {index.column()}"
-        return None
+        match role:
+            case Qt.ItemDataRole.DisplayRole:
+                if index.row() == 0 and index.column() == 1:
+                    return QTime.currentTime().toString()
+                return f"Row {index.row()}, Column {index.column()}"
+            case Qt.ItemDataRole.FontRole:
+                font = QFont()
+                font.setBold(True)
+                return font
+            case Qt.ItemDataRole.BackgroundRole:
+                if (index.row() == 0 and index.column() == 0):
+                    return QColor(Qt.GlobalColor.red)
+            case Qt.ItemDataRole.TextAlignmentRole:
+                if (index.row() == 1 and index.column() == 2):
+                    return Qt.AlignmentFlag.AlignRight
+            case Qt.ItemDataRole.CheckStateRole:
+                if (index.row() == 1 and index.column() == 3):
+                    return Qt.CheckState.Checked
+            
 
 
 if  __name__ == "__main__":
