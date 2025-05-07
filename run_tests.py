32842208130df301ecda79594a0296eb603c5e29
Bartlomiej Banas <bartlomiej.banas@cern.ch>
2025-05-07 16:22:03 +0200

    fixed the pytest parameters to skip the GUI tests on the gitlab CI

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,9 @@
 import pytest
 
 if __name__ == "__main__":
-    exit_code = pytest.main(["-s", "-v", "--color=yes", "tests/unit_tests/"])
+    exit_code = pytest.main([
+        "-s",
+        "-v",
+        "--color=yes",
+        "tests/unit_tests"])
     exit(exit_code)
