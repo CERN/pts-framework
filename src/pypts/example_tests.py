51797a750b1953b9d3da003d229d98dffb2f3055
alvaro <alvaro.martinez.landete@cern.ch>
2025-06-04 10:56:06 +0200

    licenses cleanup

--- src/pypts/example_tests.py
+++ src/pypts/example_tests.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2025 2025 CERN <home.cern>
 # SPDX-FileCopyrightText: 2025 CERN
 #
-# SPDX-License-Identifier: LGPL-2.1-or-lateer
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import logging
