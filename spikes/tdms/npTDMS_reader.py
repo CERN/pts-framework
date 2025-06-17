e68c954e4d87f6bd5fdca31b898a613f11f59d4e
Alvaro Martinez Landete <alvaro.martinez.landete@cern.ch>
2025-06-17 17:00:10 +0200

    add licences to spikes

--- spikes/tdms/npTDMS_reader.py
+++ spikes/tdms/npTDMS_reader.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 CERN <home.cern>
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
 from nptdms import TdmsFile
 import numpy as np
 import os
--- spikes/tdms/npTDMS_writer.py
+++ spikes/tdms/npTDMS_writer.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 CERN <home.cern>
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
 from nptdms import TdmsWriter, RootObject, GroupObject, ChannelObject
 import numpy as np
 
