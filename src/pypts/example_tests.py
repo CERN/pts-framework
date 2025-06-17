846d1217f0268d3baaabedcca50afdb615ada9df
Alvaro Martinez Landete <alvaro.martinez.landete@cern.ch>
2025-06-17 16:02:14 +0200

    adding sinewave test

--- src/pypts/example_tests.py
+++ src/pypts/example_tests.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import logging
+import numpy as np
 
 '''
 This file contains a collection of example tests that can be used to develop the PTS framework.
@@ -37,4 +38,31 @@ def simple_output(value):
 
 def is_PSU_disconnected():
     return (True)
+
+def generate_sinewave(frequency=60, duration=1.0):
+    """
+    Generate a sinewave with sampling rate according to Nyquist theorem.
+    
+    Args:
+        frequency: Frequency of the sinewave in Hz (default: 60)
+        duration: Duration of the signal in seconds (default: 1.0)
+    
+    Returns:
+        dict: Contains 'data' (numpy array) and 'sampling_rate' (Hz)
+    """
+    sampling_rate = frequency * 10  # 10x for Nyquist theorem
+    
+    # Generate time vector
+    t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
+    
+    # Generate 60Hz sinewave
+    data = np.sin(2 * np.pi * frequency * t)
+    
+    return {
+        "data": data,
+        "sampling_rate": sampling_rate,
+        "frequency": frequency,
+        "duration": duration,
+        "num_samples": len(data)
+    }
 
