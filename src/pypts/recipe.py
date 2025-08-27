1e46dec6386bff159caf9a4dd514a44145183cd6
Bartlomiej Banas <bartlomiej.banas@cern.ch>
2025-08-27 09:46:47 +0200

    fixed crash on invalid step input

--- src/pypts/recipe.py
+++ src/pypts/recipe.py
@@ -560,9 +560,18 @@ class Step:
             step_result.set_skip() 
         else:
             logger.info(f"Running step {self.name}")
-            step_input = self.process_inputs(runtime)
+
+            # try:
+            #     step_input = self.process_inputs(runtime)
+            # except:
+            #     logger.error(f"Error occurred while running step {self.name}. Unable to create step input")
+            #     error_info = traceback.format_exc()
+            #     step_result.set_error(error_info)
+            #     logger.error(error_info)
 
             try:
+                step_input = {}
+                step_input = self.process_inputs(runtime)
                 step_output = self._step(runtime, step_input, step_result.uuid)
             except:
                 logger.error(f"Error occurred while running step {self.name}")
