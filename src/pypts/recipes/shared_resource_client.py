b98f33b617fba750f4ce826081b34196491117cb
Bartlomiej Banas <bartlomiej.banas@cern.ch>
2025-11-05 10:54:55 +0100

    added a temporary resource handler layer

---
+++ src/pypts/recipes/shared_resource_client.py
@@ -0,0 +1,20 @@
+# resource_client.py
+import socket
+import json
+
+class RemoteDevice:
+    def __init__(self, host="127.0.0.1", port=5005):
+        self.host = host
+        self.port = port
+
+    def call(self, method, *args, **kwargs):
+        req = {"method": method, "args": args, "kwargs": kwargs}
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            s.sendall(json.dumps(req).encode())
+            resp = json.loads(s.recv(4096).decode())
+            return resp
+
+    # Convenience wrappers
+    def increment(self, n): return self.call("increment", n)
+    def get_value(self): return self.call("get_value")
---
+++ src/pypts/recipes/shared_resource_tests_example.py
@@ -0,0 +1,27 @@
+from pypts.recipes.shared_resource_server import ResourceServer
+from pypts.recipes.shared_resource_client import RemoteDevice
+
+_server = ResourceServer()
+
+def setup_resource_server():
+    # Start server
+    _server.start()
+
+def stop_resource_server():
+    # Stop server
+    _server.stop()
+
+def increment_value(how_much = 1):
+    dev = RemoteDevice()
+    return {"output": dev.increment(how_much)}
+
+def get_value():
+    dev = RemoteDevice()
+    return {"output":dev.get_value()}
+
+def simple_output(value):
+    return {"output":value + 1}
+
+def other_test():
+    # logger.info("I could also do this.")
+    return {"some_return": True, "value": 3}
