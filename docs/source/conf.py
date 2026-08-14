a045cb143141d903e9d3e967e11be0d3917f78be
alvaro <alvaro.martinez.landete@cern.ch>
2026-08-14 14:07:53 +0200

    fix docs removing _static build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,10 +48,6 @@ exclude_patterns = []
 #
 html_theme = "alabaster"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
 html_show_sphinx = False
 html_show_sourcelink = True
 
